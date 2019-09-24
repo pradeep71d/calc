d83e8c6e78fcf024950bcc528650b21ae2d0c1d3
sandeep <sandeep@ashmar.in>
2019-09-24 09:33:58 +0530

    add

---
+++ app/src/main/java/com/example/libexample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.libexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.calc.Calculation;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ calc/src/main/java/com/example/calc/Calculation.java
@@ -0,0 +1,8 @@
+package com.example.calc;
+public class Calculation {
+    public void show(){
+        String s1="this is string example";
+        String rev=new StringBuffer(s1).reverse().toString();
+
+    }
+}
